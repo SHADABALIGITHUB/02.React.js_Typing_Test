0142b20413a5350b877e75aaabfc9f7322391301
shadab <shad46243@gmail.com>
2024-07-24 15:48:16 +0530

    Typing UI and Caps lock issue

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -3,8 +3,8 @@ import Box from './Box';
 
 const Background = () => {
     return (
-        <div className='m-0 p-0 w-screen h-screen bg-gradient-to-t from-slate-400 to-slate-700 flex justify-center items-center flex-col gap-16'>
-            <h2 className='font-google1 text-4xl text-zinc-800 hover:animate-bounce transition-all duration:700 ease-in-out'>  Typing Text For 1 Minute </h2>
+        <div className='m-0 p-0 overflow-hidden w-screen h-screen bg-gradient-to-r bg-yellow-50 to-white flex justify-center items-center flex-col gap-16'>
+            <h2 className='font-google1 text-4xl text-sky-500 hover:animate-bounce transition-all duration:700 ease-in-out cursor-pointer'>  Typing Text </h2>
             <Box />
         </div>
     );
--- src/components/ColorfulText.jsx
+++ src/components/ColorfulText.jsx
@@ -15,15 +15,27 @@ const ColorfulText = ({ letters }) => {
 
 
 
+
+
+
+
   const detect = (e) => {
+    
     if(cursorPos===letters.length){
-      alert("array over here ");
+      alert("Congratulations! You have completed the typing test");
   
       
 
       return;
     }
-    if (e.key === "Backspace" || e.key === "Delete") {
+    if(e.key==="Control" || e.key==="Alt" || e.key==="Shift" || e.key==="CapsLock" || e.key==="Tab" || e.key==="Enter" || e.key==="ArrowLeft" || e.key==="ArrowRight" || e.key==="ArrowUp" || e.key==="ArrowDown" || e.key==="Meta" || e.key==="Escape" || e.key==="PageUp" || e.key==="PageDown" || e.key==="End" || e.key==="Home" || e.key==="Insert" || e.key==="Delete" || e.key==="F1" || e.key==="F2" || e.key==="F3" || e.key==="F4" || e.key==="F5" || e.key==="F6" || e.key==="F7" || e.key==="F8" || e.key==="F9" || e.key==="F10" || e.key==="F11" || e.key==="F12" || e.key==="ScrollLock" || e.key==="Pause" || e.key==="ContextMenu" || e.key==="PrintScreen" || e.key==="NumLock" || e.key==="Clear" || e.key==="MediaPlayPause" || e.key==="MediaStop" || e.key==="MediaTrackNext" || e.key==="MediaTrackPrevious" || e.key==="MediaFastForward" || e.key==="MediaRewind" || e.key==="MediaRecord" || e.key==="MediaEject" || e.key==="MediaVolumeUp" || e.key==="MediaVolumeDown" || e.key==="MediaVolumeMute" || e.key==="MediaPlay" || e.key==="MediaPause" || e.key==="MediaSelect" || e.key==="MediaLast" || e.key==="BrowserBack" || e.key==="BrowserForward" || e.key==="BrowserRefresh" || e.key==="BrowserStop" || e.key==="BrowserSearch" || e.key==="BrowserFavorites" || e.key==="BrowserHome" || e.key==="VolumeMute" || e.key==="VolumeDown" || e.key==="VolumeUp" || e.key==="LaunchApp1" || e.key==="LaunchApp2" || e.key==="LaunchMail" || e.key==="LaunchMediaSelect" || e.key==="LaunchMusicPlayer" || e.key==="LaunchApplication1"){
+     
+      return;
+
+    
+    }
+    
+      if (e.key === "Backspace" || e.key === "Delete") {
       setCursorPos(prev => {
         return prev > 0 ? prev - 1 : 0;
       })
@@ -34,7 +46,7 @@ const ColorfulText = ({ letters }) => {
       setColor(prev => {
         const newColor = [...prev];
         if (letters[cursorPos] === e.key) {
-          newColor[cursorPos] = '#00FF00';
+          newColor[cursorPos] = '#5BBED2';
         } else {
           newColor[cursorPos] = '#FF0000';
         }
@@ -46,7 +58,7 @@ const ColorfulText = ({ letters }) => {
     
     }
 
-    console.log(cursorPos);
+    // console.log(cursorPos);
     
 
 
@@ -57,16 +69,12 @@ const ColorfulText = ({ letters }) => {
     }
   }
 
+  // window.addEventListener('keydown', detect);
+
   return (
 
     <>
-  
-<button onKeyDown={detect} type="button" className="fixed bottom-10 right-1/2 text-gray-900 bg-gradient-to-r
- from-lime-200 via-lime-400 to-lime-500 hover:bg-gradient-to-br
- focus:ring-4 focus:outline-none focus:ring-lime-300
-  dark:focus:ring-lime-800 font-medium rounded-lg 
-  text-sm px-5 py-2.5 text-center me-2 mb-2"> Start </button>
-    <div tabIndex="0">
+   <div tabIndex="0" className=''>
  
       <audio ref={audioRef} controls={true} className='hidden'>
         <source src="/Audio1.mp3" type="audio/mpeg" />
@@ -74,11 +82,17 @@ const ColorfulText = ({ letters }) => {
       </audio>
 
       {letters.map((letter, index) => (
-        <span key={index} style={{ color: color[index] }} >
+        <div className='inline text-3xl font-serif text-justify leading-10 underline text-pretty' key={index} style={{ color: color[index] }} >
           {letter}
-        </span>
+        </div>
       ))}
     </div>
+
+    <button onKeyDown={detect} type="button" className="fixed bottom-10 right-1/2 text-gray-900 bg-gradient-to-r
+ from-lime-200 via-lime-400 to-lime-500 hover:bg-gradient-to-br
+ focus:ring-4 focus:outline-none focus:ring-lime-300
+  dark:focus:ring-lime-800 font-medium rounded-lg 
+  text-sm px-5 py-2.5 text-center me-2 mb-2"> Start </button>
   </>
   );
 }
